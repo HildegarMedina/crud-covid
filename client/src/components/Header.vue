58b4e65f76a0ff4355ce5beda56b9ff8ca611563
Hildegar Medina <hildegar@visie.com.br>
2021-07-31 10:30:27 -0300

    Header simple  culminado

---
+++ client/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="header-main bg-primary">
+        <div class="container">
+            <div class="row mx-0 p-3">
+                <span class="fw-bold text-white">
+                    CRUD CODIV
+                </span>
+            </div>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
